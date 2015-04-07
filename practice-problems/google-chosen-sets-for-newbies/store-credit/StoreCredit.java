58d2642eea063181b6b781f05c10b93bf4a533f2
Karthikraj Duraisamy <raj.karthik777@gmail.com>
2015-04-07 12:00:47 +0530

    Update StoreCredit.java

--- practice-problems/google-chosen-sets-for-newbies/store-credit/StoreCredit.java
+++ practice-problems/google-chosen-sets-for-newbies/store-credit/StoreCredit.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class StoreCredit {
+public class A {
 	private PrintWriter out;
 	private Scanner in;
 	private String testCase = "A-large-practice";
@@ -14,7 +14,7 @@ public class StoreCredit {
 	
 	public static void main(String[] args) throws Exception {
 		long startTime = System.nanoTime();
-		new StoreCredit().solve();
+		new A().solve();
 		long endTime = System.nanoTime();
 		long durationInMs = TimeUnit.MILLISECONDS.convert((endTime - startTime), TimeUnit.NANOSECONDS);
 		System.out.println("Running Time : "+ durationInMs +" milliseconds"); 
